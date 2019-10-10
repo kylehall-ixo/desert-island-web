62e39c6733ff1e4e890888a2e2c46a42fdf10cc6
Kyle Hall <kyle.d.hall@accenture.com>
2019-10-10 15:22:04 -0500

    revert last two commits

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import React from 'react'
-import { ApplicationInsights } from '@microsoft/applicationinsights-web'
-import {
-  ReactPlugin,
-  withAITracking
-} from '@microsoft/applicationinsights-react-js'
-import { createBrowserHistory } from 'history'
 
-const browserHistory = createBrowserHistory({ basename: '' })
-var reactPlugin = new ReactPlugin()
-var appInsights = new ApplicationInsights({
-  config: {
-    instrumentationKey: '16d23ada-0548-4048-ab90-537e190279ef',
-    extensions: [reactPlugin],
-    extensionConfig: {
-      [reactPlugin.identifier]: { history: browserHistory }
-    }
-  }
-})
-appInsights.loadAppInsights()
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
+const App = props => (
+    <div>
         <h1>Welcome the to the Big Desert Island project!</h1>
-      </div>
-    )
-  }
-}
+    </div>
+)
 
-export default withAITracking(reactPlugin, appInsights, App)
+export default App
