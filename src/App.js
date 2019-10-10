bbd784df87155393bb1e282572a410a99109bb0e
Kyle Hall <kyle.d.hall@accenture.com>
2019-10-10 14:41:34 -0500

    add app insights

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+import {
+  ReactPlugin,
+  withAITracking
+} from '@microsoft/applicationinsights-react-js'
+import { createBrowserHistory } from 'history'
 
-const App = props => (
+const browserHistory = createBrowserHistory({ basename: '' })
+var reactPlugin = new ReactPlugin()
+var appInsights = new ApplicationInsights({
+  config: {
+    instrumentationKey: '16d23ada-0548-4048-ab90-537e190279ef',
+    extensions: [reactPlugin],
+    extensionConfig: {
+      [reactPlugin.identifier]: { history: browserHistory }
+    }
+  }
+})
+appInsights.loadAppInsights()
+
+class App extends React.Component {
+  render() {
     <div>
-        <h1>Welcome the to the Big Desert Island project!</h1>
+      <h1>Welcome the to the Big Desert Island project!</h1>
     </div>
-)
+  }
+}
 
-export default App
+export default withAITracking(reactPlugin, appInsights, App)
